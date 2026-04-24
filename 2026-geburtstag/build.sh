4a28711a711e711575f52408dfd36012cd5f6b46
Uli Heller <uli.heller@daemons-point.com>
2026-04-24 12:03:49 +0200

    Zwischenstand

--- 2026-geburtstag/build.sh
+++ 2026-geburtstag/build.sh
@@ -1,6 +1,7 @@
-pandoc --standalone index.md >index.html
-pandoc --standalone start.md >start.html
-pandoc --standalone esel.md  >esel.html
-pandoc --standalone vier.md  >vier.html
-pandoc --standalone ichputzegernesonntags.md  >ichputzegernesonntags.html
-
+pandoc --standalone index.md                 >index.html
+pandoc --standalone start.md                 >start.html
+pandoc --standalone esel.md                  >esel.html
+pandoc --standalone vier.md                  >vier.html
+pandoc --standalone ichputzegernesonntags.md >ichputzegernesonntags.html
+pandoc --standalone wandertrainingslager.md  >wandertrainingslager.html
+pandoc --standalone nixplatt.md              >nixplatt.html
