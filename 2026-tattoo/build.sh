0fb41efbe60415ae9cd3d699d8da54853ab03b2c
Uli Heller <uli.heller@daemons-point.com>
2026-07-31 09:27:27 +0200

    HTML

--- 2026-tattoo/build.sh
+++ 2026-tattoo/build.sh
@@ -1,2 +1,8 @@
-pandoc --standalone index.md                 >index.html
-pandoc --standalone studio.md                >studio.html
+#!/bin/sh
+D="$(dirname "${0}")"
+
+(
+    cd "${D}"
+    pandoc --standalone index.md                 >index.html
+    pandoc --standalone studio.md                >studio.html
+)
